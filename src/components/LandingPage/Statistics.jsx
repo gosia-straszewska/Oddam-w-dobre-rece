5ebf0f1e2668baeb47cda22c985ceeaa3cd11ff1
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-11 00:39:57 +0200

    steps - content only

--- src/components/LandingPage/Statistics.jsx
+++ src/components/LandingPage/Statistics.jsx
@@ -13,7 +13,7 @@ export class Statistics extends Component {
                     <div className="stats-info">
                     <p className="stats-info-numb">5</p>
                     <p className="stats-info-title">Wspartych organizacji</p>
-                    <p className="stats-info-txt">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, nisi. Nulla totam alias in perspiciatis.</p>
+                    <p className="stats-info-txt">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste, nisi. Nulla totam alias in.</p>
                     </div>
                     <div className="stats-info">
                     <p className="stats-info-numb">7</p>
--- src/components/LandingPage/Steps.jsx
+++ src/components/LandingPage/Steps.jsx
@@ -1,9 +1,42 @@
 import React, { Component } from 'react';
+import {Link as LinkRouter} from 'react-router-dom'
 
 export class Steps extends Component {
     render() {
         return (
-            <div className="steps" id="steps">Steps</div>
+            <div className="steps" id="steps">
+                <div className="container">
+                    <div className="steps-title">
+                        <p>Wystarczą 4 proste kroki</p>
+                        {/* img */}
+                    </div>
+                    <div className="steps-roadmap">
+                        <div className="steps-roadmap-step">
+                            {/* img */}
+                            <p className="steps-roadmap-step-title">Wybierz rzeczy</p>
+                            <p className="steps-roadmap-step-txt">Ubrania, zabawki, sprzęt i inne</p>
+                        </div>
+                        <div className="steps-roadmap-step">
+                            {/* img */}
+                            <p className="steps-roadmap-step-title">Spakuj je</p>
+                            <p className="steps-roadmap-step-txt">Skorzystaj z worków na śmieci</p>
+                        </div>
+                        <div className="steps-roadmap-step">
+                            {/* img */}
+                            <p className="steps-roadmap-step-title">Zdecyduj komu chcesz pomóc</p>
+                            <p className="steps-roadmap-step-txt">Wybierz zaufane miejsce</p>
+                        </div>
+                        <div className="steps-roadmap-step">
+                            {/* img */}
+                            <p className="steps-roadmap-step-title">Zamów kuriera</p>
+                            <p className="steps-roadmap-step-txt">Kurier przyjedzie w dogodnym terminie</p>
+                        </div>
+                    </div>
+                    <div className="steps-cta">
+                        <LinkRouter to="/logowanie"><button className="steps-cta-btn">oddaj rzeczy</button></LinkRouter>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
