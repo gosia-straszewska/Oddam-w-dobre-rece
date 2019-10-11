880b0c5b6ac38899b4dac06d4525c1343ed5780f
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-11 20:18:41 +0200

    contact form - basics + background

--- src/components/LandingPage/Contact.jsx
+++ src/components/LandingPage/Contact.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react';
+import decoration from '../../assets/Decoration.svg';
 
 export class Contact extends Component {
     render() {
         return (
-            <div id="contact" className="contact">Contact</div>
+            <div id="contact" className="contact">
+            <div className="contact-img">
+                Pusty
+            </div>
+            <div className="contact-container">
+                <p className="contact-title"> Skontaktuj się z nami</p>
+                <img src={decoration} alt="" />
+                <div className="contact-form">
+                <form action="">
+                    <label htmlFor="">Wpisz swoje imię
+                    <input type="text"/>
+                    </label>
+                    <label htmlFor="">Wpisz swój email
+                    <input type="email"/>
+                    </label>
+                    <label htmlFor="">Wpisz swoją wiadomość
+                    <textarea name="" id="" cols="30" rows="10">
+                    </textarea>
+                    </label>
+                    <input type="submit" value="Wyślij" />
+                </form>
+                </div>
+            </div>
+            
+            
+            </div>
         )
     }
 }
