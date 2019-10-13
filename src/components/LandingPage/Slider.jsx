fa9e1d2ff7fc7aa79ece1ee82a2a70915ebb7aa3
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-13 13:44:14 +0200

    slider - basic scaffold

--- src/components/LandingPage/Slider.jsx
+++ src/components/LandingPage/Slider.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
+import decoration from '../../assets/Decoration.svg';
 
 export class Slider extends Component {
     render() {
         return (
-            <div className="slider" id="organizations">Slider</div>
+            <div className="slider" id="organizations">
+            <div className="container">
+                <div className="slider-title">
+                    <p>Komu pomagamy?</p>
+                    <img src={decoration} alt="" />
+                </div>
+                <div className="slider-nav">
+                    <button className="slider-nav-btn">Fundacjom</button>
+                    <button className="slider-nav-btn">Organizacjom pozarządowym</button>
+                    <button className="slider-nav-btn">Lokalnym zbiórkom</button>
+                </div>
+                <div className="slider-info">
+                    <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z<br></br>którymi współpracujemy. Możesz sprawdzić czym się zajmują,<br></br>komu pomagają i czego potrzebują.</p>
+                </div>
+                <div className="slider-list">
+                    lista
+                </div>
+            </div>
+            </div>
         )
     }
 }
