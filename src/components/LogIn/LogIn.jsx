d5d8b9a030ce98b788fe16ef9ec9e7a94c413828
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-19 22:20:12 +0200

    login validation

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -6,7 +6,41 @@ import {Link as LinkRouter} from 'react-router-dom';
 
 
 export class LogIn extends Component {
+    state = {
+        mail: '',
+        password: '',
+        mailError: '',
+        passwordError: '',
+    }
+    
+
+    handleChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({
+          [name]: value
+        });
+    };
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log(`mail=${this.state.mail} pass= ${this.state.password}`);
+
+        if(!this.state.mail.includes('@')){
+            this.setState({
+                mailError: 'Podany email jest nieprawidłowy',
+            })
+        } if(this.state.password.length <= 5 ){
+            this.setState({
+                passwordError: 'Podane hasło jest za krótkie!',
+            })
+        } return console.log(this.state)
+        
+        };
+
     render() {
+        const { mail, password, mailError, passwordError} = this.state;
+
         return (
             <div>
                 <Navigation />
@@ -17,23 +51,26 @@ export class LogIn extends Component {
                             <img src={decoration} alt="" />
                         </div>
 
-                        <form action="" className="log-in-form">
+                        <form action="" className="log-in-form" onSubmit={this.handleSubmit}>
                             <div className="log-in-form-details">
                                 <div className="log-in-form-mail">
                                     <label htmlFor="">Email</label>
-                                    <input type="email" />
+                                    <input type="text" value={mail} onChange={this.handleChange} name="mail"/>
+                                    {/* type - zmienione na text zamiast email do walidacji i wyświetlenia błędu */}
+                                    <div className="log-in-input-error">{mailError}</div>
                                 </div>
                                 <div className="log-in-form-password">
                                     <label htmlFor="">Hasło</label>
-                                    <input type="password" />
+                                    <input type="password" value={password} onChange={this.handleChange} name="password"/>
+                                    <div className="log-in-input-error">{passwordError}</div>
                                 </div>
                             </div>
                             <div className="log-in-btns">
                                 <div className="log-in-form-btn-register">
-                                    <LinkRouter to='/rejestracja'><input type="" value="Załóż konto"/></LinkRouter>
+                                    <LinkRouter to='/rejestracja'><input type="" defaultValue="Załóż konto"/></LinkRouter>
                                 </div>
                                 <div className="log-in-form-btn-login">
-                                    <input type="" value="Zaloguj się"/>
+                                    <input type="submit" value="Zaloguj się"/>
                                 </div>
                             </div>
                         </form>
