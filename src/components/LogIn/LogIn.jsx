0636bee511a15cc50d084343e1087b2d57205636
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-20 13:54:04 +0200

    LogIn validation change - clear form when it's validate

--- src/components/LogIn/LogIn.jsx
+++ src/components/LogIn/LogIn.jsx
@@ -1,46 +1,59 @@
 import React, { Component } from 'react';
 import { Navigation } from '../LandingPage/Navigation';
 import decoration from '../../assets/Decoration.svg';
-import {Link as LinkRouter} from 'react-router-dom';
+import { Link as LinkRouter } from 'react-router-dom';
 
 const initialState = {
-            mail: '',
-            password: '',
-            mailError: '',
-            passwordError: '',
+    mail: '',
+    password: '',
+    mailError: '',
+    passwordError: '',
 }
 
 export class LogIn extends Component {
-        state = initialState;
-        
-    
-        handleChange = e => {
-            const value = e.target.value;
-            const name = e.target.name;
+    state = initialState;
+
+
+    handleChange = e => {
+        const value = e.target.value;
+        const name = e.target.name;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    validate = () => {
+        if (!this.state.mail.includes('@')) {
             this.setState({
-              [name]: value
+                mailError: 'Podany email jest nieprawidłowy',
             });
-        };
-    
-        handleSubmit = e => {
-            e.preventDefault();
-            console.log(`mail=${this.state.mail} pass= ${this.state.password}`);
-            console.log(this.state);
-    
-            if(!this.state.mail.includes('@')){
-                this.setState({
-                    mailError: 'Podany email jest nieprawidłowy',
-                })
-            } if(this.state.password.length <= 5 ){
-                this.setState({
-                    passwordError: 'Podane hasło jest za krótkie!',
-                })
-            } else return console.log("zarejestrowano")
-            
-            };
+            return false
+        }
+        if (this.state.password.length <= 5) {
+            this.setState({
+                passwordError: 'Podane hasło jest za krótkie!',
+            })
+            return false
+        }
+        return true
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const isValid = this.validate();
+        if (isValid) {
+            console.log(this.state, "zalogowano")
+            //clear the form
+            this.setState(
+                initialState
+            );
+
+        } else console.log("nie zalogowano")
+    };
 
     render() {
-        const { mail, password, mailError, passwordError} = this.state;
+        const { mail, password, mailError, passwordError } = this.state;
 
         return (
             <div>
@@ -56,22 +69,22 @@ export class LogIn extends Component {
                             <div className="log-in-form-details">
                                 <div className="log-in-form-mail">
                                     <label htmlFor="">Email</label>
-                                    <input type="text" value={mail} onChange={this.handleChange} name="mail"/>
+                                    <input type="text" value={mail} onChange={this.handleChange} name="mail" />
                                     {/* type - zmienione na text zamiast email do walidacji i wyświetlenia błędu */}
                                     <div className="log-in-input-error">{mailError}</div>
                                 </div>
                                 <div className="log-in-form-password">
                                     <label htmlFor="">Hasło</label>
-                                    <input type="password" value={password} onChange={this.handleChange} name="password"/>
+                                    <input type="password" value={password} onChange={this.handleChange} name="password" />
                                     <div className="log-in-input-error">{passwordError}</div>
                                 </div>
                             </div>
                             <div className="log-in-btns">
                                 <div className="log-in-form-btn-register">
-                                    <LinkRouter to='/rejestracja'><input type="" defaultValue="Załóż konto"/></LinkRouter>
+                                    <LinkRouter to='/rejestracja'><input type="" defaultValue="Załóż konto" /></LinkRouter>
                                 </div>
                                 <div className="log-in-form-btn-login">
-                                    <input type="submit" value="Zaloguj się"/>
+                                    <input type="submit" value="Zaloguj się" />
                                 </div>
                             </div>
                         </form>
