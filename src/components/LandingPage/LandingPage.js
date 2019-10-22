a299741ee01c264ccdfa9723d9a02b122a88925e
Gosia Straszewska <malgorzata.straszewska@gmail.com>
2019-10-22 14:58:38 +0200

    Slider - onClick added - to show list

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -3,7 +3,7 @@ import { Navigation } from './Navigation';
 import { Welcome } from './Welcome';
 import { About } from './About';
 import { Contact } from './Contact';
-import { Slider } from './Slider';
+import { Slider} from './Slider';
 import { Statistics } from './Statistics';
 import {Steps} from './Steps';
 // import { LogIn} from '../LogIn/LogIn';
--- src/components/LandingPage/Slider.jsx
+++ src/components/LandingPage/Slider.jsx
@@ -2,51 +2,153 @@ import React, { Component } from 'react';
 import decoration from '../../assets/Decoration.svg';
 // import ReactPaginate from 'react-paginate';
 
-class List extends Component {
+class FundationList extends Component {
     constructor(props) {
-                super(props);
-                this.state = {
-                    items: [],
-                    isLoaded: false,
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
 
-                }
+        }
     };
 
-    componentDidMount(){
-        
+    componentDidMount() {
+
         fetch("http://localhost:3000/fundations")
-                .then(res => res.json())
-                .then(json => {
-                    this.setState ({
-                        isLoaded: true,
-                        items: json,
-                    })
-                }); 
-            };
-    
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    };
+
     render() {
 
-        var {isLoaded, items} = this.state;
+        var { isLoaded, items } = this.state;
 
-        if(!isLoaded){
+        if (!isLoaded) {
             return <div>Wczytywanie...</div>
         } else {
 
             return (
 
-            <div>
-                {items.map(item => (
-                    <li className="list-el" key={item.id}>
-                        <div className="list-el-about">
-                            <p className="list-el-title">{item.name}</p>
-                            <p className="list-el-goal">{item.goal}</p>
-                        </div>
-                        <div className="list-el-things">
-                            <p>{item.things}</p>
-                        </div>
-                    </li>
-                ))}
-            </div>
+                <div>
+                    {items.map(item => (
+                        <li className="list-el" key={item.id}>
+                            <div className="list-el-about">
+                                <p className="list-el-title">{item.name}</p>
+                                <p className="list-el-goal">{item.goal}</p>
+                            </div>
+                            <div className="list-el-things">
+                                <p>{item.things}</p>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+
+            )
+        }
+    }
+}
+
+export class OrganizationsList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+
+        }
+    };
+
+    componentDidMount() {
+
+        fetch("http://localhost:3000/organizations")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    };
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Wczytywanie...</div>
+        } else {
+
+            return (
+
+                <div>
+                    {items.map(item => (
+                        <li className="list-el" key={item.id}>
+                            <div className="list-el-about">
+                                <p className="list-el-title">{item.name}</p>
+                                <p className="list-el-goal">{item.goal}</p>
+                            </div>
+                            <div className="list-el-things">
+                                <p>{item.things}</p>
+                            </div>
+                        </li>
+                    ))}
+                </div>
+
+            )
+        }
+    }
+}
+
+export class LocalList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [],
+            isLoaded: false,
+
+        }
+    };
+
+    componentDidMount() {
+
+        fetch("http://localhost:3000/local")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json,
+                })
+            });
+    };
+
+    render() {
+
+        var { isLoaded, items } = this.state;
+
+        if (!isLoaded) {
+            return <div>Wczytywanie...</div>
+        } else {
+
+            return (
+
+                <div>
+                    {items.map(item => (
+                        <li className="list-el" key={item.id}>
+                            <div className="list-el-about">
+                                <p className="list-el-title">{item.name}</p>
+                                <p className="list-el-goal">{item.goal}</p>
+                            </div>
+                            <div className="list-el-things">
+                                <p>{item.things}</p>
+                            </div>
+                        </li>
+                    ))}
+                </div>
 
             )
         }
@@ -54,31 +156,67 @@ class List extends Component {
 }
 
 export class Slider extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: 1
+        }
+        this.handleFundationsClick = this.handleFundationsClick.bind(this);
+        this.handleOrganizationsClick = this.handleOrganizationsClick.bind(this);
+        this.handleLocalClick = this.handleLocalClick.bind(this)
 
-render() {
-    return (
-        <div className="slider" id="organizations">
-            <div className="container">
-                <div className="slider-title">
-                    <p>Komu pomagamy?</p>
-                    <img src={decoration} alt="" />
-                </div>
-                <div className="slider-nav">
-                    <button className="slider-nav-btn">Fundacjom</button>
-                    <button className="slider-nav-btn">Organizacjom<br></br>pozarządowym</button>
-                    <button className="slider-nav-btn">Lokalnym<br></br>zbiórkom</button>
-                </div>
-                <div className="slider-info">
-                    <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z<br></br>którymi współpracujemy. Możesz sprawdzić czym się zajmują,<br></br>komu pomagają i czego potrzebują.</p>
-                </div>
-                <div className="slider-list">
-                    <ul className="slider-list-elements" ref="list">
-                        <List/>
-                        
-                    </ul>
+    }
+    handleFundationsClick() {
+        this.setState({
+            show: 1
+        })
+    }
+
+    handleOrganizationsClick() {
+        this.setState({
+            show: 2
+        })
+    }
+
+    handleLocalClick() {
+        this.setState({
+            show: 3
+        })
+    }
+
+    render() {
+        let showList;
+
+        if (this.state.show === 1) {
+            showList = <FundationList />
+        } if (this.state.show === 2) {
+            showList = <OrganizationsList />
+        } if (this.state.show === 3) {
+            showList = <LocalList />
+        }
+
+        return (
+            <div className="slider" id="organizations">
+                <div className="container">
+                    <div className="slider-title">
+                        <p>Komu pomagamy?</p>
+                        <img src={decoration} alt="" />
+                    </div>
+                    <div className="slider-nav">
+                        <button className="slider-nav-btn" onClick={this.handleFundationsClick}>Fundacjom</button>
+                        <button className="slider-nav-btn" onClick={this.handleOrganizationsClick}>Organizacjom<br></br>pozarządowym</button>
+                        <button className="slider-nav-btn" onClick={this.handleLocalClick}>Lokalnym<br></br>zbiórkom</button>
+                    </div>
+                    <div className="slider-info">
+                        <p>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z<br></br>którymi współpracujemy. Możesz sprawdzić czym się zajmują,<br></br>komu pomagają i czego potrzebują.</p>
+                    </div>
+                    <div className="slider-list">
+                        <ul className="slider-list-elements" ref="list">
+                            {showList}
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+        )
+    }
 }
